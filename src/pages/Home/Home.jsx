a1a56946d38c71a1ad5365fc628cafaf999be6c8
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-07-01 20:37:43 +0300

    fixing react player

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -27,12 +27,12 @@ export const Home = () => {
       </>
       ) : (
         <Section>
-        {/* <ReactPlayer
-          url='https://youtu.be/ORrqkh9LvHA'
+        <ReactPlayer
+          url='https://media.istockphoto.com/id/1404978793/uk/%D0%B2%D1%96%D0%B4%D0%B5%D0%BE/%D0%B2%D0%B8%D0%B4-%D0%B7-%D0%BF%D0%BE%D0%B2%D1%96%D1%82%D1%80%D1%8F-%D0%BD%D0%B0-%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D0%B5-%D0%BF%D0%B0%D1%81%D0%BE%D0%B2%D0%B8%D1%89%D0%B5-%D0%BD%D0%B0-%D1%82%D0%BB%D1%96-%D0%BD%D0%B0%D0%B9%D0%B2%D0%B8%D1%89%D0%BE%D1%97-%D0%B3%D1%80%D1%83%D0%B7%D0%B8%D0%BD%D1%81%D1%8C%D0%BA%D0%BE%D1%97-%D0%B3%D0%BE%D1%80%D0%B8-%D1%88%D1%85%D0%B0%D1%80%D0%B0-%D0%BF%D0%BE%D0%B1%D0%BB%D0%B8%D0%B7%D1%83-%D1%83%D1%88%D0%B3%D1%83%D0%BB%D1%96-%D0%B2.mp4?s=mp4-640x640-is&k=20&c=LEKa8ZXcZfN8YHEotUK5LoQNGKwz3ATJYzXzlgDn_Fo='
           width='500px'
           height='300px'
           controls
-          /> */}
+          />
         </Section>
       )}
       ;
