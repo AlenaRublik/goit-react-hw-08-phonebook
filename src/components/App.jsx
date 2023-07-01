96bb631948d0857a78661bf99b4c1962e6309f3f
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-07-01 20:30:45 +0300

    code refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,54 @@
-import { ContactForm, Filter, Contacts } from 'components/index';
-import { H1, H2 } from './App/App.styled';
+import { useEffect, lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
+import RestrictedRoute from './RestrictedRoute/RestrictedRoute';
+import { refreshUser } from 'redux/auth/authOperations';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export function App() {
+const Home = lazy(() => import('pages/Home/Home'));
+const Register = lazy(() => import('pages/Register/Register'));
+const Login = lazy(() => import('pages/Login/Login'));
+const Contacts = lazy(() => import('pages/Contacts/Contacts'));
+
+export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
     <>
-      <H1>PhoneBook</H1>
-      <ContactForm />
-      <H2>Contacts</H2>
-      <Filter />
-      <Contacts />
-      <ToastContainer/>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<Register />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<Contacts />} />
+            }
+          />
+        </Route>
+      </Routes>
+      <ToastContainer />
     </>
   );
-}
+};
